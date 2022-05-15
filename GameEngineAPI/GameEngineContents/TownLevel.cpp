c5640a25a89f26c0a13beb6adda3b474347bda10
LeeJungmin <apple0220@naver.com>
2022-05-15 12:33:59 +0900

    이정민 : 디버그모드 컬리전 추가

--- GameEngineAPI/GameEngineContents/TownLevel.cpp
+++ GameEngineAPI/GameEngineContents/TownLevel.cpp
@@ -2,6 +2,7 @@
 #include "Player.h"
 #include "ContentsEnums.h"
 #include <GameEngine/GameEngineRenderer.h>
+#include <GameEngineBase/GameEngineInput.h>
 
 TownLevel::TownLevel() 
 {
@@ -10,6 +11,8 @@ TownLevel::TownLevel()
 	BGBuilding_ = CreateActor<BackGround>((int)ORDER::BUIDING);
 	BGFront_ = CreateActor<BackGround>((int)ORDER::FRONTA);
 	BGAlwaysFront_ = CreateActor<BackGround>((int)ORDER::ALWAYSFRONT);
+	BGCollisionMap_ = CreateActor<BackGround>((int)ORDER::COLLISION);
+	BGCollisionMap_->Off();
 
 	// 이미지의 좌상단이 0,0이 되도록
 	BackGround_->GetRenderer()->SetPivot({ MAP_TOWN_SIZE_W / 2, MAP_TOWN_SIZE_H / 2 });
@@ -24,6 +27,9 @@ TownLevel::TownLevel()
 
 	BGAlwaysFront_->GetRenderer()->SetPivot({ MAP_TOWN_SIZE_W / 2, MAP_TOWN_SIZE_H / 2 });
 	BGAlwaysFront_->GetRenderer()->SetImage(MAP_TOWN_ALWAYSFRONT);
+
+	BGCollisionMap_->GetRenderer()->SetPivot({ MAP_TOWN_SIZE_W / 2, MAP_TOWN_SIZE_H / 2 });
+	BGCollisionMap_->GetRenderer()->SetImage(MAP_TOWN_RENDER_ORDER_COLLISION);
 
 	// 타일의 랜더를 저장
 	this->Player_->SetTileMap(&BackGround_->TileMap_);
@@ -65,5 +71,26 @@ void TownLevel::Update()
 	else
 	{
 		BGFront_->GetRenderer()->GameEngineRenderer::SetOrder((int)ORDER::FRONTB);
+	}
+
+
+	if (true == GameEngineInput::GetInst()->IsDown(KEY_DEBUG))
+	{
+		GameEngineLevel::IsDebugModeSwitch();
+	}
+
+	if (true == GameEngineInput::GetInst()->IsDown(KEY_COLLISIONMAP_UP)
+		&& false == ColMapFlg_)
+	{
+		BGCollisionMap_->GetRenderer()->SetOrder((int)ORDER::FRONTB);
+		BGCollisionMap_->On();
+		ColMapFlg_ = true;
+	}
+	else if (true == GameEngineInput::GetInst()->IsDown(KEY_COLLISIONMAP_UP)
+		&& true == ColMapFlg_)
+	{
+		BGCollisionMap_->GetRenderer()->SetOrder((int)ORDER::COLLISION);
+		BGCollisionMap_->Off();
+		ColMapFlg_ = false;
 	}
 }
